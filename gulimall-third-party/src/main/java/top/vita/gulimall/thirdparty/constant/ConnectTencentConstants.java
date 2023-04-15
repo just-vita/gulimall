142abaa0778557ca1b1f9d4d013bc9698aded903
just-vita <1254581982@qq.com>
2023-04-15 20:14:09 +0800

    Update ConnectTencentConstants.java

--- gulimall-third-party/src/main/java/top/vita/gulimall/thirdparty/constant/ConnectTencentConstants.java
+++ gulimall-third-party/src/main/java/top/vita/gulimall/thirdparty/constant/ConnectTencentConstants.java
@@ -1,14 +1,14 @@
 package top.vita.gulimall.thirdparty.constant;
 
 public class ConnectTencentConstants {
-    public static final String SECRETID = "AKIDTlob6KJ3XkghS5ewDF1ZAYgZ5LM6ZlgL";
+    public static final String SECRETID = "xx";
 
-    public static final String SECRETKEY = "SeZqvlNjqiLhuxoyvhPwCCwPfgQt5zd2";
+    public static final String SECRETKEY = "xx";
 
-    public static final String BUCKETNAME = "learn-1312191491";
+    public static final String BUCKETNAME = "x-x";
 
-    public static final String APCITY = "ap-beijing";
+    public static final String APCITY = "ap-x";
 
-    public static final String TENCENT_PATH = "https://learn-1312191491.cos.ap-beijing.myqcloud.com";
+    public static final String TENCENT_PATH = "https://xxx";
 
 }
